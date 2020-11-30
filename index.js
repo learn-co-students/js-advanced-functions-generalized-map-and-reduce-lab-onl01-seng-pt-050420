4b24181f27545d758209126f95c3864223adb75a
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-30 10:02:43 +0000

    Done.

--- index.js
+++ index.js
@@ -9,18 +9,23 @@ function map(inputArray, callbackFn)
   return newArray;
 }
 
-function reduce(inputArray, callbackFn, accumulator) {
-  const keys = Object.keys(inputArray)
-  let i = 1
+function reduce(inputArray, callbackFn, accumulator) 
+{
+  const keys = Object.keys(inputArray);
+  let i = 1;
 
-  if (accumulator) {
-    i = 0
-  } else {
-    accumulator = inputArray[keys[0]]
+  if (accumulator) 
+  {
+    i = 0;
+  } 
+  else 
+  {
+    accumulator = inputArray[keys[0]];
   }
 
-  for (i; i < keys.length; i++) {
-    accumulator = callbackFn(accumulator, inputArray[keys[i]])
+  for (i; i < keys.length; i++) 
+  {
+    accumulator = callbackFn(accumulator, inputArray[keys[i]]);
   }
-  return accumulator
+  return accumulator;
 }
