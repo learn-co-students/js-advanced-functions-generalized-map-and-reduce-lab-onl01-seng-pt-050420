0e5092ba4fe38a041904788d6f5f2be5f6ec1676
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-30 10:00:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(inputArray, callbackFn)
+{
+  let newArray = [];
+  for(const element of inputArray)
+  {
+    newArray.push(callbackFn(element));
+  }
+  return newArray;
+}
+
+function reduce(inputArray, callbackFn, accumulator) {
+  const keys = Object.keys(inputArray)
+  let i = 1
+
+  if (accumulator) {
+    i = 0
+  } else {
+    accumulator = inputArray[keys[0]]
+  }
+
+  for (i; i < keys.length; i++) {
+    accumulator = callbackFn(accumulator, inputArray[keys[i]])
+  }
+  return accumulator
+}
