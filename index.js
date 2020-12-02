b473f2f7fee54ad9c5a5083b386330088a70fba1
kanu-e <chukwuemekakanu@gmail.com>
2020-12-02 14:43:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+var numbers = [-65, -44, -12, -4];
+
+function map(sourceArray, fn){
+    let array = [];
+
+    for (const elem of sourceArray) {
+        let result = fn(elem);
+        array.push(result);
+    };
+
+    return array;
+};
+
+function reduce(sourceArray, fn, start){
+    let r = (!!start) ? start : sourceArray[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < sourceArray.length; i++) {
+      r = fn(sourceArray[i], r)
+    }
+  
+    return r;
+}
