fb7934b8d61cc9df9e56a414926588e0e681c27a
Fiveolioo <96richardohc@gmail.com>
2020-12-01 23:02:45 -1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+const map = function(arr, func) {
+    let newArr = []
+    for (let i=0; i<arr.length; i++) {
+        newArr.push(func(arr[i]))
+    }
+    return newArr
+}
+
+const reduce = function(arr, func, startingPoint) {
+    let value = startingPoint;
+    for (let i=0; i<arr.length; i++) {
+        if (!value) {
+            value = arr[i]
+        } else {
+            value = func(arr[i], value)
+        }
+    }
+    return value || false
+}
