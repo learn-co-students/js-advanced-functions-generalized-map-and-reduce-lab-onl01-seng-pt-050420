3e0ef1917ef5b8ae767af02a49efb8baacbd9c46
Jonathan East <jonathan_east@ymail.com>
2020-12-09 20:43:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+const map = function(source, callback) {
+  let result = [];
+  for(let i = 0; i < source.length; i ++) {
+    let element = source[i];
+    result.push(callback(element));
+  }
+  return result;
+}
+
+
+const reduce = function(source, callback, startingPoint) {
+  let result = (!!startingPoint) ? startingPoint : source[0];
+  let i = (!!startingPoint) ? 0 : 1;
+  
+  for(; i < source.length; i++) {
+    result = callback(source[i], result)
+  }
+  return result;
+}
